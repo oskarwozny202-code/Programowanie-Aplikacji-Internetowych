d98b2ad89c67e0612f818d8def52b15849c834a0
OsiB <oskarwozny202@gmail.com>
2026-09-24 12:12:48 +0200

    Dodanie plikow

---
+++ continue.php
@@ -0,0 +1,6 @@
+for( $i=0; $i<10; $i++) {
+    if( $i == 5 )
+        continue;
+    echo "liczba: $i <br>";
+}
+echo "koniec pętli";
---
+++ goto.php
@@ -0,0 +1,7 @@
+<?php
+
+    goto a;
+    echo "pierwszy tekst";
+
+a: //tzw. etykieta
+    echo "drugi tekst";
