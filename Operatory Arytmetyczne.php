9dce2157ee0a62a2da9fa47de001f1657597364a
OsiB <oskarwozny202@gmail.com>
2026-09-24 11:55:15 +0200

    Dodanie plikow

---
+++ Operatory Arytmetyczne.php
@@ -0,0 +1,10 @@
+<?php
+    $a=5;
+    $b=3;
+    echo "liczby a=$a, liczba b=$b <br>";
+    echo "suma a+b=" .  ($a + $b)  . "<br>";  //8
+    echo "roznica a-b=" .  ($a - $b)  . "<br>";  //2
+    echo "iloczyn a*b=" .  ($a * $b)  . "<br>";  //15
+    echo "iloraz a/b=" .  ($a / $b)  . "<br>";  //1.6666
+    echo "modulo a%b=" .  ($a % $b)  . "<br>";  //2
+    echo "a do potęgi b =" .  ($a**$b)  . "<br>";  //125
